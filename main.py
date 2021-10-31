8345479fceef2833681565a9fbc386dafd214f11
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-10-31 21:42:54 +0100

    change samesite cookie setting

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def report_view(siteid:str,request: Request,response: Response, pageid:Optional[
             print('found uid:',request.cookies['uid'])
             query['user']=request.cookies['uid']
         res = collection.update_one(query,{'$push': {'visits': visit}},upsert=True)
-        response.set_cookie(key='uid',value=query['user'],domain="ziapp.de",samesite="lax",expires=365*24*60*60, httponly=True,secure=True)
+        response.set_cookie(key='uid',value=query['user'],domain="ziapp.de",samesite="None",expires=365*24*60*60, httponly=True,secure=True)
         if (filename=='counter.png'):
             return FileResponse("counter.png")
         else:
