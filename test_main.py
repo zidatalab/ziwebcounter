1569de90cb408e4fa8bd3142e6afb6072a226827
anjaseidel <aseidel@zi.de>
2021-11-01 14:49:45 +0100

    help with those tests, closes #4

--- test_main.py
+++ test_main.py
@@ -13,17 +13,16 @@ def test_1_test_status():
     res = client.get("/")
     assert res.status_code == 200
     
-# def test_2_test_view_without_img():
-#     '''
-#     Checks if View can be submitted
-#     '''
-#     res = client.get("/view/testname/data?pageid=/builtest.html")
-#     assert res.status_code == 200
+def test_2_test_view_without_img():
+    '''
+    Checks if View can be submitted
+    '''
+    res = client.get("/view/testname/data?pageid=/builtest.html")
+    assert res.status_code == 200
 
-# def test_3_test_view_with_img():
-#     '''
-#     Checks if View can be submitted
-#     '''
-#     res = client.get("/view/testname/counter.png?pageid=/builtest.html")
-#     print(res)
-#     assert res.status_code == 200
+def test_3_test_view_with_img():
+    '''
+    Checks if View can be submitted
+    '''
+    res = client.get("/view/testname/counter.png?pageid=/builtest.html")
+    assert res.status_code == 200
